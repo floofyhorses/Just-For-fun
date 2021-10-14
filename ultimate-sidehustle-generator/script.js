558c4ff6dbcbf2b55b30bbd150b43ce1df262baf
growthosedendrites <69527252+growthosedendrites@users.noreply.github.com>
2021-10-14 11:57:28 -0700

    Add files via upload

---
+++ ultimate-sidehustle-generator/script.js
@@ -0,0 +1,20 @@
+	
+
+
+
+	function coolSideHustle() {
+		var type = ["Professional", "Super", "Executive", "Seasonal", "Home-based", "Musical", "Assistant", "Associate", "Junior", "Senior", "V.I.P.", "Glorified", "Designated", "Temporary"];
+		var thing = ["hot dog", "dog", "cat", "jewelry", "handicrafts", "cupcake", "computer", "money", "crochet", "online", "toys", "comic", "book", "house", "hair", "webcam", "pickle"];
+		var doer = ["processor", "maker", "washer", "purveyor", "seller", "marketer", "programmer", "taster", "concierge", "curator", "accountant", "manager", "inspector", "moderator", "advisor"];
+
+	
+	    var rand1 = Math.floor(Math.random() * type.length);
+	    var rand2 = Math.floor(Math.random() * thing.length);
+	    var rand3 = Math.floor(Math.random() * doer.length);
+
+	    var sideHustle = `${type[rand1]} ${thing[rand2]} ${doer[rand3]}`;
+	    alert(sideHustle);
+
+	}
+
+	coolSideHustle();
