3bfd7173667503529ec6e87072021922d1abf939
growthosedendrites <69527252+growthosedendrites@users.noreply.github.com>
2021-10-17 16:41:42 -0700

    Update script.js

--- ultimate-sidehustle-generator/script.js
+++ ultimate-sidehustle-generator/script.js
@@ -4,8 +4,8 @@
 
 	function coolSideHustle() {
 		var type = ["Professional", "Super", "Executive", "Seasonal", "Home-based", "Musical", "Assistant", "Associate", "Junior", "Senior", "V.I.P.", "Glorified", "Designated", "Temporary"];
-		var thing = ["hot dog", "dog", "cat", "jewelry", "handicrafts", "cupcake", "computer", "money", "crochet", "online", "toys", "comic", "book", "house", "hair", "webcam", "pickle"];
-		var doer = ["processor", "maker", "washer", "purveyor", "seller", "marketer", "programmer", "taster", "concierge", "curator", "accountant", "manager", "inspector", "moderator", "advisor"];
+		var thing = ["hot dog", "poodle", "cat", "jewelry", "handicrafts", "cupcake", "computer", "money", "crochet", "online", "toys", "comic", "book", "house", "hair", "webcam", "pickle"];
+		var doer = ["person","processor", "maker", "washer", "purveyor", "seller", "marketer", "programmer", "taster", "concierge", "curator", "accountant", "manager", "inspector", "moderator", "advisor"];
 
 	
 	    var rand1 = Math.floor(Math.random() * type.length);
